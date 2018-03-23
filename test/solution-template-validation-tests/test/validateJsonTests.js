98a8dc5b997e64a29c09a27a41a9abbfca1ce01e
Jianping Zeng <jizen@microsoft.com>
2018-03-23 16:41:04 -0700

    Move tests in grunt.js to mocha test

--- test/solution-template-validation-tests/test/validateJsonTests.js
+++ test/solution-template-validation-tests/test/validateJsonTests.js
@@ -5,12 +5,36 @@ var util = require('./util');
 const filesFolder = './';
 var path = require('path');
 var chai = require('chai');
+//var assert = chai.assert; // Using Assert style
+//var expect = chai.expect; // Using Expect style
 var jsonlint = require('jsonlint');
 var should = chai.should();
 
 var folder = process.env.npm_config_folder || filesFolder;
 
 var jsonFiles = util.getFiles(folder, '.json');
+
+/** Validates every AMP solution must have a top-level mainTemplate.json template and a createUIDefinition.json. */
+describe('a top-level mainTemplate.json and a createUIDefinition.json exist in folder- ', () => {
+    it('maintemplate.json must exist', () => {
+        try {
+            var fileString = fs.readFileSync(folder + '\\mainTemplate.json', {
+                encoding: 'utf8'
+            }).trim();
+        } catch(e) {
+            should.fail(null, null, ' Every AMP solution must have a top-level mainTemplate.json. ');
+        }
+    });
+    it('createUIDefinition.json must exist', () => {
+        try {
+            var fileString = fs.readFileSync(folder + '\\createUIDefinition.json', {
+                encoding: 'utf8'
+            }).trim();
+        } catch(e) {
+            should.fail(null, null, ' Every AMP solution must have a createUIDefinition.json. ');
+        }
+    });
+});
 
 /** Validates all json files in the given folder are valid. */
 describe('json files in folder - ', () => {
